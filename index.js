762f5aaae02bc291c2f450d1f21e2792c7cf2d61
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-09-11 12:37:38 +1000

    playing with functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Follow along with the examples here
+
+function doNothing() {};
+function sayHello() {
+    console.log("Hello!");
+  };
+  sayHello();
+
+function sayHelloRenan() {
+    console.log("Hey mate!")
+  };
+  sayHelloRenan();
+function sayHiToLaura() {
+    console.log("Hi Lorinha!")
+  };
+  sayHiToLaura();
+ function sayHiMalake (){
+    console.log("Hello Malaka !")
+  };
+  sayHiMalake();
+function hello (person) {
+    console.log(`Hello, ${person} !`)
+};
+hello("Renan");
+hello("Laura");
+hello("Douglas");
+function testingName (test,name) {
+    console.log(`let's ${test}, ${name}`);
+    console.log(" this is a test :", test);
+    console.log(" Test name", name)
+};
+testingName("yes test","Renan");
+function add(x, y) {
+    return x + y;
+  };
+console.log(add(1,2));
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+    
+  };
+  console.log(say("Howdy", "partner"));
